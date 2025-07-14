92edf44604376824389209a790e88804a38d2d5a
caromarecco <caromarecco@gmail.com>
2025-07-13 21:08:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 const materias = [
-  { codigo: 'MIBCM', nombre: 'Biología Celular y Molecular', previas: [] },
-  { codigo: 'MIBES', nombre: 'Bioestadística', previas: [] },
-  { codigo: 'MSPHB', nombre: 'Salud y Humanidades', previas: [] },
-  { codigo: 'MAT1', nombre: 'Aprendizaje en Territorio 1', previas: [] },
+  { codigo: 'MIBCM', nombre: 'Biología Celular y Molecular', anio: 1, previas: [] },
+  { codigo: 'MIBES', nombre: 'Bioestadística', anio: 1, previas: [] },
+  { codigo: 'MSPHB', nombre: 'Salud y Humanidades', anio: 1, previas: [] },
+  { codigo: 'MAT1', nombre: 'Aprendizaje en Territorio 1', anio: 1, previas: [] },
 
-  { codigo: 'MBCM', nombre: 'Biología Celular y Molecular 2', previas: ['MIBCM'] },
-  { codigo: 'MAT2', nombre: 'Aprendizaje en Territorio 2', previas: ['MAT1'] },
+  { codigo: 'MBCM', nombre: 'Biología Celular y Molecular 2', anio: 2, previas: ['MIBCM'] },
+  { codigo: 'MAT2', nombre: 'Aprendizaje en Territorio 2', anio: 2, previas: ['MAT1'] },
 
-  { codigo: 'MANAT', nombre: 'Anatomía (CBCC2)', previas: ['MBCM'] },
-  { codigo: 'MHBIO', nombre: 'Histología y Biofísica', previas: ['MBCM'] },
+  { codigo: 'MANAT', nombre: 'Anatomía (CBCC2)', anio: 3, previas: ['MSPHB'] },
+  { codigo: 'MHBIO', nombre: 'Histología y Biofísica', anio: 3, previas: ['MBCM'] },
 
-  { codigo: 'HIST', nombre: 'Histología (Neuro/Cardio)', previas: ['MBCM'] },
-  { codigo: 'BCC3N', nombre: 'Neurociencias', previas: ['MBCM'] },
-  { codigo: 'BCC4C', nombre: 'Cardiovascular y Respiratorio', previas: ['MBCM'] },
+  { codigo: 'HIST', nombre: 'Histología Neuro/Cardio', anio: 4, previas: ['MBCM'] },
+  { codigo: 'BCC3N', nombre: 'Neurociencias', anio: 4, previas: ['MBCM'] },
+  { codigo: 'BCC4C', nombre: 'Cardio y Respiratorio', anio: 4, previas: ['MBCM'] },
 
-  { codigo: 'BCC5', nombre: 'Digestivo, Renal, Endocrino y Reproductivo', previas: ['MBCM', 'MANAT'] },
+  { codigo: 'BCC5', nombre: 'Digestivo y Endocrino', anio: 5, previas: ['MBCM', 'MANAT'] },
+  { codigo: 'BCC6', nombre: 'Hematología e Inmunobio', anio: 5, previas: ['MBCM'] },
 
-  { codigo: 'BCC6', nombre: 'Hematología e Inmunobio.', previas: ['MBCM'] },
-  { codigo: 'MC1', nombre: 'Metodología Científica 1', previas: ['MIBES', 'BCC5'] },
+  { codigo: 'MC1', nombre: 'Metodología Científica 1', anio: 6, previas: ['MIBES', 'BCC5'] },
+  { codigo: 'M4PNA', nombre: 'Medicina 1er Nivel Atención', anio: 6, previas: ['BCC5', 'BCC6'] },
+  { codigo: 'M4BCP', nombre: 'Bases Científicas Patología', anio: 6, previas: ['BCC5', 'BCC6'] },
 
-  { codigo: 'M4PNA', nombre: 'Medicina 1er Nivel Atención', previas: ['BCC5', 'BCC6'] },
-  { codigo: 'M4BCP', nombre: 'Bases Científicas de la Patología', previas: ['BCC5', 'BCC6'] },
+  { codigo: 'M4PED', nombre: 'Pediatría', anio: 7, previas: ['M4PNA', 'M4BCP'] },
+  { codigo: 'M4GYN', nombre: 'Ginecología y Neonatología', anio: 7, previas: ['M4PNA', 'M4BCP'] },
 
-  { codigo: 'M4PED', nombre: 'Pediatría', previas: ['M4PNA', 'M4BCP'] },
-  { codigo: 'M4GYN', nombre: 'Ginecología y Neonatología', previas: ['M4PNA', 'M4BCP'] },
+  { codigo: 'MCM', nombre: 'Clínica Médica', anio: 8, previas: ['M4PED', 'M4GYN'] },
+  { codigo: 'MPMT', nombre: 'Patología Médica y Terapéutica', anio: 8, previas: ['M4BCP'] },
 
-  { codigo: 'MCM', nombre: 'Clínica Médica', previas: ['M4PED', 'M4GYN'] },
-  { codigo: 'MPMT', nombre: 'Patología Médica y Terapéutica', previas: ['M4BCP'] },
+  { codigo: 'M6CQ', nombre: 'Clínica Quirúrgica', anio: 9, previas: ['MCM', 'M4BCP', 'M4PNA'] },
+  { codigo: 'M6PQ', nombre: 'Patología Quirúrgica', anio: 9, previas: ['M4BCP'] },
+  { codigo: 'M6MFC', nombre: 'Salud Mental y Psicología Médica', anio: 9, previas: ['M4PNA'] },
+  { codigo: 'MC2', nombre: 'Metodología Científica 2', anio: 9, previas: ['MC1', 'M4BCP', 'M4PNA'] },
 
-  { codigo: 'M6CQ', nombre: 'Clínica Quirúrgica', previas: ['MCM'] },
-  { codigo: 'M6PQ', nombre: 'Patología Quirúrgica', previas: ['M4BCP'] },
-  { codigo: 'M6MFC', nombre: 'Salud Mental / Psicología Médica', previas: ['M4PNA'] },
-  { codigo: 'MC2', nombre: 'Metodología Científica 2', previas: ['MC1', 'M4BCP', 'M4PNA'] },
-
-  { codigo: 'INTO', nombre: 'Internado Obligatorio', previas: ['M6CQ', 'M6PQ', 'M6MFC', 'MC2'] }
+  { codigo: 'INTO', nombre: 'Internado Obligatorio', anio: 10, previas: ['M6CQ', 'M6PQ', 'M6MFC', 'MC2'] }
 ];
 
 const malla = document.getElementById('malla');
@@ -63,6 +62,7 @@ function crearMalla() {
     const div = document.createElement('div');
     div.className = 'materia';
     div.setAttribute('data-codigo', m.codigo);
+    div.setAttribute('data-ano', m.anio);
     div.innerHTML = `<strong>${m.codigo}</strong><br>${m.nombre}`;
 
     if (m.previas.length > 0) {
